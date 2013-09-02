b5e026a03f213387bfaa2e57e7782de715ddfebd
cponeill <caseyoneill78@hotmail.com>
2013-09-02 09:25:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
-	var pathname = url.parse(request.url).pathname;
-	console.log("Let's keep it going with " + pathname);
+        var pathname = url.parse(request.url).pathname;
+        console.log("Let's keep it going with " + pathname);
 
-	route(pathname);
+        route(handle, pathname);
 
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World and all that Jazz!");
-	response.end();
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World and all that Jazz!");
+        response.end();
     }
     http.createServer(onRequest).listen(8888);
     console.log("Engines are starting up now!");
 }
 
 exports.start = start;
+
+
