d42bffc1e251d6da2a28e2662e3d702bbd741a8a
cponeill <caseyoneill78@hotmail.com>
2013-09-04 12:41:03 -0700

    Update requestHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,14 @@
 function start() {
+    // Console.log prints the phrase
     console.log("Request handler 'start' was called.");
     
+    // The sleep function
     function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliseconds)
+        var startTime = new Date().getTime(); // The variable startTime is assigned the current date and time
+        while (new Date().getTime() < startTime + milliseconds) // While the current time is less than the startTime plus milliseconds, start() will be called. 
     }
     
+    // When start() is called, Node.js waits 10 secondes until it returns "Hello Start".
     sleep(10000);
     return "Hello Start";
 }
