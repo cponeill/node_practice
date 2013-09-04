3e22b23b17cf5f430a3549bb0de599144faecf29
cponeill <caseyoneill78@hotmail.com>
2013-09-04 12:44:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var http = require("http");
 var url = require("url");
 
-function start(route, handle) {
-    function onRequest(request, response) {
+function start(route, handle) { // the function start calls route and handle
+    function onRequest(request, response) { // the onRequest function will hellp start the server.
         var pathname = url.parse(request.url).pathname;
         console.log("Let's keep it going with " + pathname);
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.writeHead(200, {"Content-Type": "text/plain"}); // This assigns the type and text
         var content = route(handle, pathname);
-        response.write("Hello World and all that Jazz!");
-        response.end();
+        response.write("Hello World and all that Jazz!"); // This writes our the text on the web page.
+        response.end(); // This ends the server. There is no more information to be received.
     }
-    http.createServer(onRequest).listen(8888);
-    console.log("Engines are starting up now!");
+    http.createServer(onRequest).listen(8888); // The server is created and we can access it locally at localhost:8888
+    console.log("Engines are starting up now!"); // This prints out in the terminal and lets me know the server has started and works correctly. 
 }
 
 exports.start = start;
